33cc4f5d0d8335f7cf0fe20fdc227defadb5c5b6
Israilla <israillacaroline@hotmail.com>
2020-05-11 17:05:43 -0300

    completed

---
+++ Exercise02_demo_2/Exercise02 (demo 2)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Exercise02__demo_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string path = @"c:\temp\file1.txt";
+                StreamReader sr = null;
+                try
+                {
+                    sr = File.OpenText(path);
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine("An error occurred");
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    if (sr != null) sr.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Exercise05/Exercise05/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Exercise05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\myfolder";
+
+            try
+            {
+                var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS: ");
+                foreach (string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES: ");
+                foreach(string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+
+                Directory.CreateDirectory(@"c:\temp\TCC\newfolder");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
